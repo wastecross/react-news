8239fdfbe72ee82bfe82408cae16215665938f0a
wastecross <charly_1310@outlook.com>
2020-10-07 16:26:04 -0500

    feat: add url
    Add href to link a new tab.

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,5 +1,5 @@
-import React, { Component } from "react";
-import "./Cards.css";
+import React, { Component } from 'react';
+import './Cards.css';
 
 class Cards extends Component {
   constructor(props) {
@@ -11,25 +11,29 @@ class Cards extends Component {
       content: props.content,
       image: props.image,
       author: props.author,
+      url: props.url,
     };
   }
 
   componentDidMount() {
     this.setState({
-      content: this.state.content.substring(
-        0,
-        Math.min(this.state.content.length, 10)
-      ) + '...',
+      content:
+        this.state.content.substring(
+          0,
+          Math.min(this.state.content.length, 10)
+        ) + '...',
     });
   }
 
   render() {
     return (
       <div className="Cards">
-        <img className="Cards-image" src={this.state.image} alt="news" />
-        <p className="Cards-p">{this.state.author}</p>
-        <h4 className="Cards-h4">{this.state.title}</h4>
-        <p className="Cards-p">{this.state.content}</p>
+        <a href={this.state.url} target="blank">
+          <img className="Cards-image" src={this.state.image} alt="news" />
+          <p className="Cards-p">{this.state.author}</p>
+          <h4 className="Cards-h4">{this.state.title}</h4>
+          <p className="Cards-p">{this.state.content}</p>
+        </a>
       </div>
     );
   }
