6717851d4cbab9954ece1f45d6a4df7af23a93fc
wastecross <charly_1310@outlook.com>
2020-10-06 23:59:55 -0500

    feat('News'): add new functions
    -Get data from api.
    -Send data to component card.

--- src/containers/News/News.js
+++ src/containers/News/News.js
@@ -1,8 +1,37 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './News.css';
+import Cards from '../../components/Cards';
+import axios from 'axios';
 
-const News = (props) => {
-  return(<div className="News">Test</div>)
+const News = () => {
+  const [data, setData] = useState({});
+
+  useEffect(() => {
+    axios
+      .get(
+        'http://newsapi.org/v2/everything?q=apple&from=2020-09-15&to=2020-10-15&sortBy=popularity&apiKey=d6736e08a3ac4859aeda69469b97d4cc'
+      )
+      .then((response) => {
+        setData(response);
+      });
+  }, [setData]);
+
+  const info = data.data;
+
+  const cards =
+    data.status === 200 ? (
+      info.articles.map((news) => (
+        <Cards title={news.title} image={news.urlToImage} author={news.author} content={news.content} />
+      ))
+    ) : (
+      <span> Error </span>
+    );
+
+  return (
+    <div className='News'>
+      <div className='News-container'>{cards}</div>
+    </div>
+  );
 };
 
 export default News;
