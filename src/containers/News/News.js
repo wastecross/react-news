341cc8703b7a3fdae0817b2d28bcc6dd95f1f86e
wastecross <charly_1310@outlook.com>
2021-01-29 13:51:53 -0600

    refactor: add cards into a folder

--- src/containers/News/News.js
+++ src/containers/News/News.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import './News.css';
-import Cards from '../../components/Cards';
+import Cards from '../../components/Cards/Cards';
 import { newsArticles } from '../../fixtures/news.fixture';
 
 const News = () => {
