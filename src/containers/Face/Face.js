3277b4a61ad83508cb15a43f7d732432871554a7
wastecross <charly_1310@outlook.com>
2021-01-29 13:30:50 -0600

    feat('Face'): add face container

---
+++ src/containers/Face/Face.js
@@ -0,0 +1,25 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import "./Face.css";
+
+const Face = () => {
+  const [data, setData] = useState({});
+
+  useEffect(() => {
+    axios
+      .get(
+        "https://newsapi.org/v2/everything?q=apple&from=2020-09-15&to=2020-10-15&sortBy=popularity&apiKey=d6736e08a3ac4859aeda69469b97d4cc"
+      )
+      .then((response) => {
+        setData(response);
+      });
+  }, [setData]);
+
+  return (
+    <div className="Face">
+      <div className="Face-container"></div>
+    </div>
+  );
+};
+
+export default Face;
