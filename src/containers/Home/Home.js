d08f31e538334a9c5c6378cfe0f25023114396fb
wastecross <charly_1310@outlook.com>
2021-02-05 16:58:30 -0600

    refactor('Home.js'): change class component into function

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -1,19 +1,17 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './Home.css';
 import logo from '../../assets/logo-news.png';
 import { labels } from '../../fixtures/home.fixture';
 
-class Home extends Component {
-  render() {
-    return (
-      <div className="Home">
-        <header className="Home-header">
-          <img src={logo} className="Home-logo" alt="logo" />
-          <p>{labels.welcomeNews}</p>
-        </header>
-      </div>
-    );
-  }
-}
+const Home = () => {
+  return (
+    <div className="Home">
+      <header className="Home-header">
+        <img src={logo} className="Home-logo" alt="logo" />
+        <p>{labels.welcomeNews}</p>
+      </header>
+    </div>
+  );
+};
 
 export default Home;
