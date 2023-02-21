bec59eb0f54a14f40f31f6f7d9c667d90a6b30c3
Charly MI <charly_1310@outlook.com>
2023-02-21 14:22:10 -0600

    feat(Sdk.js): change way to display sdk

--- src/containers/Sdk/Sdk.js
+++ src/containers/Sdk/Sdk.js
@@ -18,34 +18,12 @@ const Sdk = () => {
 
   const onClickHandler = async () => {
     try {
-      const url =
-        "https://veridocid.azure-api.net/api/id/v3/createVerification";
-      const privateKey = "";
       const publicApiKey = "";
-      const response = await fetch(url, {
-        method: "POST",
-        headers: {
-          "x-api-key": privateKey,
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          id: Math.random(),
-          options: { checks: { selfie: true }, redirect_url: "" },
-        }),
-      });
-
-      if (response.ok) {
-        const responseText = await response.text();
-        const vdid = new WebVerification(publicApiKey);
-        const url = vdid.getUrlToVerifyIdentity(responseText);
-        setContainer(
-          <iframe
-            src={url}
-            title="Sdk VDID"
-            allow="camera; microphone"
-          ></iframe>
-        );
-      }
+      const vdid = new WebVerification(publicApiKey);
+      const url = vdid.getUrlToOnlyCaptureImages();
+      setContainer(
+        <iframe src={url} title="Sdk VDID" allow="camera; microphone"></iframe>
+      );
     } catch (error) {
       console.log(error);
       setContainer(<p style={{ color: "red" }}>ERROR</p>);
