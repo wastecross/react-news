96e5b1e21e5cc9b81b6c87b139b8c74eb36372d7
Charly MI <charly_1310@outlook.com>
2024-04-04 11:16:34 -0600

    feat(Upscaler): add css and js files

---
+++ src/containers/Upscaler/Upscaler.js
@@ -0,0 +1,25 @@
+import React from "react";
+import Upscaler from "upscaler";
+import image from "../../assets/templates/CaseErrorFront_01.jpg";
+import "./Upscaler.css";
+
+const UpscalerCmpnt = () => {
+  const onClickHandler = () => {
+    const upscaler = new Upscaler();
+    upscaler.upscale(image).then((upscaledImage) => {
+      console.log(upscaledImage); // base64 representation of image src
+    });
+  };
+
+  return (
+    <div className="Upscaler">
+      <div className="Upscaler-header">
+        <button className="Upscaler-button" onClick={onClickHandler}>
+          Upscaler image
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default UpscalerCmpnt;
