273bd9e3825345a00d5b0645ed99016973b38451
Charly MI <charly_1310@outlook.com>
2025-01-15 13:15:21 -0600

    refactor(Home.js): update way to set labels and logos

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -5,43 +5,26 @@ import logoFace from "../../assets/logo-face.png";
 import logoConvert from "../../assets/logo-convert.png";
 import logoSdk from "../../assets/logo-sdk.png";
 import logoUpscaler from "../../assets/logo-upscaler.png";
+import logoKey from "../../assets/logo-keys.png";
 import logo from "../../assets/logo-welcome.png";
 import { labels } from "../../fixtures/home.fixture";
 
 const Home = (props) => {
+  const logos = {
+    news: logoNews,
+    face: logoFace,
+    convert: logoConvert,
+    sdk: logoSdk,
+    upscaler: logoUpscaler,
+    key: logoKey,
+    home: logo,
+  };
+
   return (
     <div className="Home">
       <header className="Home-header">
-        <img
-          src={
-            props.type === "news"
-              ? logoNews
-              : props.type === "face"
-              ? logoFace
-              : props.type === "convert"
-              ? logoConvert
-              : props.type === "sdk"
-              ? logoSdk
-              : props.type === "upscaler"
-              ? logoUpscaler
-              : logo
-          }
-          className="Home-logo"
-          alt="logo"
-        />
-        <p>
-          {props.type === "news"
-            ? labels.welcomeNews
-            : props.type === "face"
-            ? labels.welcomeFace
-            : props.type === "convert"
-            ? labels.welcomeConvert
-            : props.type === "sdk"
-            ? labels.welcomeSdk
-            : props.type === "upscaler"
-            ? labels.welcomeUpscaler
-            : labels.welcome}
-        </p>
+        <img src={logos[props.type]} className="Home-logo" alt="logo" />
+        <p>{labels[props.type]}</p>
       </header>
     </div>
   );
