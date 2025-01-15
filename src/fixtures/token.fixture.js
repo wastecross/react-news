e4abc70c15f2cffce4b13933e096df0c200dcae5
Charly MI <charly_1310@outlook.com>
2025-01-15 13:46:31 -0600

    feat(token.ficture.js): add necessary values to get it

---
+++ src/fixtures/token.fixture.js
@@ -0,0 +1,5 @@
+export const urlToken = "http://localhost:3000/token";
+export const grant_type = "client_credentials";
+export const audience = "veridocid";
+export const client_secret = "";
+export const client_id = "";
