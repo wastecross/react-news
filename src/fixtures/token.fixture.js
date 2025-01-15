d87767da7096b428a5f30d1ced5b4f19d02461a4
Charly MI <charly_1310@outlook.com>
2025-01-15 17:44:13 -0600

    feat(token.fixture.js): update url token

--- src/fixtures/token.fixture.js
+++ src/fixtures/token.fixture.js
@@ -1,4 +1,4 @@
-export const urlToken = "http://localhost:3000/token";
+export const urlToken = "https://veridocid.azure-api.net/api/auth/token";
 export const grant_type = "client_credentials";
 export const audience = "veridocid";
 export const client_secret = "";
